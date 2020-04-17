48efdf641d1eb124f688834c772f643be8d82681
Marcin Pas <marcin.pas@softelnet.com>
2020-04-17 20:25:07 +0200

    Record MVP.

---
+++ lib/src/flutter/ui/mvp/widgets/type/record_mvp.dart
@@ -0,0 +1,176 @@
+// Copyright 2020 The Sponge authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:sponge_client_dart/sponge_client_dart.dart';
+import 'package:sponge_flutter_api/src/common/service/application_service.dart';
+import 'package:sponge_flutter_api/src/common/ui/mvp/mvp.dart';
+import 'package:sponge_flutter_api/src/common/util/model_utils.dart';
+import 'package:sponge_flutter_api/src/common/util/type_gui_utils.dart';
+import 'package:sponge_flutter_api/src/flutter/service/flutter_application_service.dart';
+import 'package:sponge_flutter_api/src/flutter/ui/context/ui_context.dart';
+import 'package:sponge_flutter_api/src/flutter/ui/type_gui_provider/type_gui_provider.dart';
+
+class RecordViewModel extends BaseViewModel {
+  RecordViewModel(this.uiContext);
+
+  final UiContext uiContext;
+}
+
+abstract class RecordView extends BaseView {}
+
+class RecordPresenter extends BasePresenter<RecordViewModel, RecordView> {
+  RecordPresenter(
+      ApplicationService service, RecordViewModel model, RecordView view)
+      : super(service, model, view);
+
+  Map<String, TypeGuiProvider> _typeGuiProviders;
+  bool _isExpanded;
+
+  @override
+  FlutterApplicationService get service =>
+      FlutterApplicationService.of(super.service);
+
+  bool isExpanded() {
+    if (_isExpanded == null) {
+      _isExpanded =
+          !uiContext.qualifiedType.type.nullable && uiContext.value != null ||
+              uiContext.value != null;
+    } else if (_isExpanded && uiContext.value == null) {
+      _isExpanded = false;
+    }
+
+    return _isExpanded;
+  }
+
+  RecordType get recordType => uiContext.qualifiedType.type as RecordType;
+
+  Map<String, TypeGuiProvider> get typeGuiProviders {
+    _typeGuiProviders ??= {
+      for (var field in recordType.fields)
+        field.name: service.getTypeGuiProvider(field)
+    };
+
+    return _typeGuiProviders;
+  }
+
+  UiContext get uiContext => viewModel.uiContext;
+
+  bool get isRecordReadOnly => uiContext.readOnly;
+
+  bool get isRecordEnabled => uiContext.enabled;
+
+  bool get isValueNotSet => DataTypeUtils.isValueNotSet(uiContext.value);
+
+  bool get shouldShowNullValue => isRecordReadOnly && isValueNotSet;
+
+  bool get isRoot => uiContext.qualifiedType.isRoot;
+
+  QualifiedDataType get qualifiedRecordType => uiContext.qualifiedType;
+
+  Map get record => uiContext.value as Map;
+
+  bool get isThisRootRecordSingleLeadingField =>
+      uiContext.rootRecordSingleLeadingField != null && isRoot;
+
+  /// Show context actions only for normal records (i.e. not for a logical record
+  /// that represents the action args).
+  bool get shouldShowContextActions => !isRoot;
+
+  bool get shouldShowExpandCheckbox =>
+      uiContext.qualifiedType.type.nullable ||
+      DataTypeUtils.isValueNotSet(uiContext.value);
+
+  bool get shouldEnableExpandCheckbox => isRecordEnabled && !isRecordReadOnly;
+
+  dynamic getFieldValue(QualifiedDataType qFieldType) {
+    Validate.notNull(
+        record, 'The record ${qualifiedRecordType.type.name} must not be null');
+    return record[qFieldType.type.name];
+  }
+
+  bool shouldFieldBeEnabled(QualifiedDataType qFieldType) =>
+      uiContext.callbacks.shouldBeEnabled(qFieldType);
+
+  bool hasRootRecordSingleLeadingField() {
+    var thisLeadingFieldPath = ModelUtils.getRootRecordSingleLeadingField(
+            uiContext.qualifiedType, uiContext.value as Map)
+        ?.qType
+        ?.path;
+
+    return uiContext.rootRecordSingleLeadingField != null &&
+        uiContext.rootRecordSingleLeadingField == thisLeadingFieldPath;
+  }
+
+  String get label => uiContext.getDecorationLabel();
+
+  void toggleExpand() {
+    _isExpanded = !_isExpanded;
+
+    if (_isExpanded) {
+      if (uiContext.value == null) {
+        var newValue = <String, dynamic>{};
+        var defaultValue = DataTypeUtils.unwrapAnnotatedValue(
+            uiContext.qualifiedType.type.defaultValue);
+
+        if (defaultValue != null) {
+          Validate.isTrue(defaultValue is Map<String, dynamic>,
+              'A default value for a record should be a map');
+          newValue = defaultValue;
+        }
+
+        uiContext.value = newValue;
+      }
+    } else {
+      uiContext.value = null;
+    }
+
+    onSave(uiContext.qualifiedType, uiContext.value);
+  }
+
+  List<List<DataType>> createFieldGroups() {
+    var groups = <List<DataType>>[];
+    String lastGroupName;
+    int lastGroupIndex = -1;
+
+    // TODO Util method - merge with annotated features.
+    recordType.fields
+        .where((fieldType) => fieldType.features[Features.VISIBLE] ?? true)
+        .toList()
+        .asMap()
+        .forEach((i, fieldType) {
+      String fieldGroup = fieldType.features[Features.GROUP];
+      if (lastGroupName != null && lastGroupName == fieldGroup) {
+        groups[lastGroupIndex].add(fieldType);
+      } else {
+        groups.add([fieldType]);
+        lastGroupIndex++;
+      }
+
+      lastGroupName = fieldGroup;
+    });
+
+    return groups;
+  }
+
+  bool hasAnyFieldInGroupScroll(List<DataType> fieldGroup) => fieldGroup
+      .any((fieldType) => DataTypeGuiUtils.hasListTypeScroll(fieldType));
+
+  void onSave(QualifiedDataType qType, dynamic value) {
+    uiContext.callbacks.onSave(qType, value);
+  }
+
+  void onUpdate(QualifiedDataType qType, dynamic value) {
+    uiContext.callbacks.onUpdate(qType, value);
+  }
+}
--- lib/src/flutter/ui/widgets/type/record_widget.dart
+++ lib/src/flutter/ui/widgets/type/record_widget.dart
@@ -14,14 +14,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:sponge_client_dart/sponge_client_dart.dart';
-import 'package:sponge_flutter_api/src/common/util/model_utils.dart';
-import 'package:sponge_flutter_api/src/common/util/type_gui_utils.dart';
 import 'package:sponge_flutter_api/src/flutter/application_provider.dart';
 import 'package:sponge_flutter_api/src/flutter/ui/context/ui_context.dart';
-import 'package:sponge_flutter_api/src/flutter/ui/type_gui_provider/type_gui_provider.dart';
+import 'package:sponge_flutter_api/src/flutter/ui/mvp/widgets/type/record_mvp.dart';
 import 'package:sponge_flutter_api/src/flutter/ui/util/gui_utils.dart';
-import 'package:sponge_flutter_api/src/flutter/ui/widgets/type_support/provided_value_set_widget.dart';
 import 'package:sponge_flutter_api/src/flutter/ui/widgets/error_widgets.dart';
+import 'package:sponge_flutter_api/src/flutter/ui/widgets/type_support/provided_value_set_widget.dart';
 import 'package:sponge_flutter_api/src/flutter/ui/widgets/type_support/sub_actions.dart';
 import 'package:sponge_flutter_api/src/flutter/ui/widgets/type_support/text_view_widget.dart';
 import 'package:sponge_flutter_api/src/flutter/ui/widgets/widgets.dart';
@@ -43,28 +41,22 @@ class RecordTypeWidget extends StatefulWidget {
   _RecordTypeWidgetState createState() => _RecordTypeWidgetState();
 }
 
-class _RecordTypeWidgetState extends State<RecordTypeWidget> {
-  Map<String, TypeGuiProvider> _typeGuiProviders;
-  bool _isExpanded;
-
-  bool get isRecordReadOnly => widget.uiContext.readOnly;
-
-  bool get isRecordEnabled => widget.uiContext.enabled;
-
-  bool _hasRootRecordSingleLeadingField() {
-    var thisLeadingFieldPath = ModelUtils.getRootRecordSingleLeadingField(
-            widget.uiContext.qualifiedType, widget.uiContext.value as Map)
-        ?.qType
-        ?.path;
-
-    return widget.uiContext.rootRecordSingleLeadingField != null &&
-        widget.uiContext.rootRecordSingleLeadingField == thisLeadingFieldPath;
-  }
+class _RecordTypeWidgetState extends State<RecordTypeWidget>
+    implements RecordView {
+  RecordPresenter _presenter;
 
   @override
   Widget build(BuildContext context) {
+    var model = RecordViewModel(widget.uiContext);
+
+    _presenter ??=
+        RecordPresenter(ApplicationProvider.of(context).service, model, this);
+
+    // The model contains the UiContext so it has to be updated every build.
+    _presenter.updateModel(model);
+
     try {
-      if (_hasRootRecordSingleLeadingField()) {
+      if (_presenter.hasRootRecordSingleLeadingField()) {
         return _buildFieldsWidgets(context)[0];
       } else {
         return _build(context);
@@ -80,21 +72,13 @@ class _RecordTypeWidgetState extends State<RecordTypeWidget> {
   }
 
   Widget _build(BuildContext context) {
-    if (_isExpanded == null) {
-      _isExpanded = !widget.uiContext.qualifiedType.type.nullable &&
-              widget.uiContext.value != null ||
-          widget.uiContext.value != null;
-    } else if (_isExpanded && widget.uiContext.value == null) {
-      _isExpanded = false;
-    }
-
-    var label = widget.uiContext.getDecorationLabel();
+    var label = _presenter.label;
+    bool isExpanded = _presenter.isExpanded();
 
     var margin = EdgeInsets.only(bottom: 5);
 
     // Return widget for null record in the read only mode.
-    if (isRecordReadOnly &&
-        DataTypeUtils.isValueNotSet(widget.uiContext.value)) {
+    if (_presenter.shouldShowNullValue) {
       return TextViewWidget(
         label: label,
         text: null,
@@ -107,20 +91,15 @@ class _RecordTypeWidgetState extends State<RecordTypeWidget> {
       children: [
         Row(
           children: [
-            if (widget.uiContext.qualifiedType.type.nullable ||
-                DataTypeUtils.isValueNotSet(widget.uiContext.value))
+            if (_presenter.shouldShowExpandCheckbox)
               IconButton(
                 key: Key('record-expand'),
-                icon: Icon(_isExpanded
+                icon: Icon(isExpanded
                     ? Icons.check_box
                     : Icons.check_box_outline_blank),
                 tooltip: 'Set nullable value',
-                onPressed: isRecordEnabled && !isRecordReadOnly
-                    ? () {
-                        setState(() {
-                          _toggleExpand();
-                        });
-                      }
+                onPressed: _presenter.shouldEnableExpandCheckbox
+                    ? () => setState(() => _presenter.toggleExpand())
                     : null,
               ),
             if (label != null)
@@ -130,12 +109,9 @@ class _RecordTypeWidgetState extends State<RecordTypeWidget> {
               ),
           ],
         ),
-        if (_isExpanded)
-          Container(
-            height: 5,
-          ),
-        if (_isExpanded) ..._buildSubActionsWidget(context),
-        if (_isExpanded)
+        if (isExpanded) Container(height: 5),
+        if (isExpanded) ..._buildSubActionsWidget(context),
+        if (isExpanded)
           OptionalExpanded(
             child: widget.uiContext.qualifiedType.isRoot
                 ? Container(
@@ -154,54 +130,27 @@ class _RecordTypeWidgetState extends State<RecordTypeWidget> {
                     ),
                     color: Theme.of(widget.uiContext.context)
                         .scaffoldBackgroundColor,
-                    shape: widget.showBorder ? ContinuousRectangleBorder(
-                      side: BorderSide(
-                        width: 1,
-                        color: getBorderColor(widget.uiContext.context),
-                      ),
-                    ) : null,
+                    shape: widget.showBorder
+                        ? ContinuousRectangleBorder(
+                            side: BorderSide(
+                              width: 1,
+                              color: getBorderColor(widget.uiContext.context),
+                            ),
+                          )
+                        : null,
                   ),
           ),
       ],
     );
   }
 
-  void _toggleExpand() {
-    _isExpanded = !_isExpanded;
-
-    if (_isExpanded) {
-      if (widget.uiContext.value == null) {
-        var newValue = <String, dynamic>{};
-        var defaultValue = DataTypeUtils.unwrapAnnotatedValue(
-            widget.uiContext.qualifiedType.type.defaultValue);
-
-        if (defaultValue != null) {
-          Validate.isTrue(defaultValue is Map<String, dynamic>,
-              'A default value for a record should be a map');
-          newValue = defaultValue;
-        }
-
-        widget.uiContext.value = newValue;
-      }
-    } else {
-      widget.uiContext.value = null;
-    }
-
-    _onSave(widget.uiContext.qualifiedType, widget.uiContext.value);
-  }
-
   List<Widget> _buildSubActionsWidget(BuildContext context) {
     var widgets = <Widget>[];
 
-    // TODO Presenter.
-    var service = ApplicationProvider.of(context).service;
-
-    // Show context actions only for normal records (i.e. not for a logical record
-    // that represents the action args).
-    if (!widget.uiContext.qualifiedType.isRoot) {
+    if (_presenter.shouldShowContextActions) {
       var subActionsWidget = SubActionsWidget.forRecord(
         widget.uiContext,
-        service.spongeService,
+        _presenter.service.spongeService,
         tooltip: 'Context actions',
       );
 
@@ -224,18 +173,9 @@ class _RecordTypeWidgetState extends State<RecordTypeWidget> {
   }
 
   List<Widget> _buildFieldsWidgets(BuildContext context) {
-    var recordType = widget.uiContext.qualifiedType.type as RecordType;
-
-    // TODO Presenter.
-    var service = ApplicationProvider.of(context).service;
-    _typeGuiProviders ??= {
-      for (var field in recordType.fields)
-        field.name: service.getTypeGuiProvider(field)
-    };
-
     var widgets = <Widget>[];
 
-    var groups = _createFieldGroups(recordType);
+    var groups = _presenter.createFieldGroups();
     groups.asMap().forEach((i, group) {
       widgets.add(_buildFieldGroupWidget(context, group));
 
@@ -247,42 +187,15 @@ class _RecordTypeWidgetState extends State<RecordTypeWidget> {
     return widgets;
   }
 
-  List<List<DataType>> _createFieldGroups(RecordType recordType) {
-    var groups = <List<DataType>>[];
-    String lastGroupName;
-    int lastGroupIndex = -1;
-
-    // TODO Util method - merge with annotated features.
-    recordType.fields
-        .where((fieldType) => fieldType.features[Features.VISIBLE] ?? true)
-        .toList()
-        .asMap()
-        .forEach((i, fieldType) {
-      String fieldGroup = fieldType.features[Features.GROUP];
-      if (lastGroupName != null && lastGroupName == fieldGroup) {
-        groups[lastGroupIndex].add(fieldType);
-      } else {
-        groups.add([fieldType]);
-        lastGroupIndex++;
-      }
-
-      lastGroupName = fieldGroup;
-    });
-
-    return groups;
-  }
-
   Widget _buildFieldGroupWidget(
       BuildContext context, List<DataType> fieldGroup) {
     Widget groupWidget;
 
     List<Widget> rawFieldWidgets = fieldGroup
         .map((fieldType) => _buildFieldWidget(
-              context,
-              widget.uiContext.qualifiedType,
-              widget.uiContext.qualifiedType.createChild(fieldType),
-              widget.uiContext.value as Map,
-            ))
+            context,
+            widget.uiContext.qualifiedType,
+            widget.uiContext.qualifiedType.createChild(fieldType)))
         .toList();
 
     if (rawFieldWidgets.length > 1) {
@@ -296,10 +209,9 @@ class _RecordTypeWidgetState extends State<RecordTypeWidget> {
     }
 
     groupWidget = Padding(
-      padding: widget.uiContext.rootRecordSingleLeadingField != null &&
-              widget.uiContext.qualifiedType.isRoot
+      padding: _presenter.isThisRootRecordSingleLeadingField
           ? const EdgeInsets.all(0)
-          : const EdgeInsets.only(left: 10, right: 10, top: 0, bottom: 0),
+          : const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
       child: Align(
         child: groupWidget,
         alignment: Alignment.centerLeft,
@@ -308,48 +220,28 @@ class _RecordTypeWidgetState extends State<RecordTypeWidget> {
 
     return
         // Expanded only if any in a group field shoud have a scroll.
-        fieldGroup.any(
-                (fieldType) => DataTypeGuiUtils.hasListTypeScroll(fieldType))
+        _presenter.hasAnyFieldInGroupScroll(fieldGroup)
             ? OptionalExpanded(child: groupWidget)
             : groupWidget;
   }
 
-  void _onSave(QualifiedDataType qType, dynamic value) {
-    widget.uiContext.callbacks.onSave(qType, value);
-  }
-
-  void _onUpdate(QualifiedDataType qType, dynamic value) {
-    widget.uiContext.callbacks.onUpdate(qType, value);
-  }
-
-  TypeEditorContext _createEditorContext(
-      QualifiedDataType qFieldType, Map record) {
-    var qRecordType = widget.uiContext.qualifiedType;
-
-    Validate.notNull(
-        record, 'The record ${qRecordType.type.name} must not be null');
-    var fieldValue = record[qFieldType.type.name];
-    var onSave = (value) => setState(() {
-          _onSave(qFieldType, value);
-        });
-    var onUpdate = (value) => setState(() {
-          _onUpdate(qFieldType, value);
-        });
-
-    var shouldFieldBeEnabled =
-        widget.uiContext.callbacks.shouldBeEnabled(qFieldType);
+  TypeEditorContext _createEditorContext(QualifiedDataType qFieldType) {
+    var shouldFieldBeEnabled = _presenter.shouldFieldBeEnabled(qFieldType);
 
     return TypeEditorContext(
       widget.uiContext.name,
       context,
       widget.uiContext.callbacks,
       qFieldType,
-      fieldValue,
+      _presenter.getFieldValue(qFieldType),
       hintText: qFieldType.type.description,
-      onSave: onSave,
-      onUpdate: onUpdate,
-      readOnly: isRecordReadOnly || !shouldFieldBeEnabled || !isRecordEnabled,
-      enabled: isRecordEnabled && shouldFieldBeEnabled,
+      onSave: (value) => setState(() => _presenter.onSave(qFieldType, value)),
+      onUpdate: (value) =>
+          setState(() => _presenter.onUpdate(qFieldType, value)),
+      readOnly: _presenter.isRecordReadOnly ||
+          !shouldFieldBeEnabled ||
+          !_presenter.isRecordEnabled,
+      enabled: _presenter.isRecordEnabled && shouldFieldBeEnabled,
       loading: widget.uiContext.loading,
       rootRecordSingleLeadingField:
           widget.uiContext.rootRecordSingleLeadingField,
@@ -357,9 +249,9 @@ class _RecordTypeWidgetState extends State<RecordTypeWidget> {
   }
 
   Widget _buildFieldWidget(BuildContext context, QualifiedDataType recordType,
-      QualifiedDataType qFieldType, Map record) {
+      QualifiedDataType qFieldType) {
     try {
-      var editorContext = _createEditorContext(qFieldType, record);
+      var editorContext = _createEditorContext(qFieldType);
 
       if (qFieldType.type.provided?.hasValueSet ?? false) {
         return AbsorbPointer(
@@ -371,22 +263,25 @@ class _RecordTypeWidgetState extends State<RecordTypeWidget> {
             widget.uiContext.callbacks.onGetProvidedArg,
             editorContext.onSave,
           ),
-          absorbing: qFieldType.type.provided.readOnly || !isRecordEnabled,
+          absorbing:
+              qFieldType.type.provided.readOnly || !_presenter.isRecordEnabled,
         );
       }
 
       var isFieldReadOnly = qFieldType.type.provided?.readOnly ?? false;
 
       // Switch to a viewer for a record field if necessary.
-      if (isRecordReadOnly || isFieldReadOnly || !isRecordEnabled) {
+      if (_presenter.isRecordReadOnly ||
+          isFieldReadOnly ||
+          !_presenter.isRecordEnabled) {
         return Padding(
           padding: EdgeInsets.only(left: 0, right: 0, top: 5, bottom: 5),
-          child: _typeGuiProviders[qFieldType.type.name]
+          child: _presenter.typeGuiProviders[qFieldType.type.name]
               .createViewer(editorContext.cloneAsViewer()),
         );
       }
 
-      return _typeGuiProviders[qFieldType.type.name]
+      return _presenter.typeGuiProviders[qFieldType.type.name]
           .createEditor(editorContext);
     } catch (e) {
       return NotificationPanelWidget(
